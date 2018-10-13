c9057826c21f66be9e98e4ac99a481d8d8a388d1
Carol Greene <carolgreene3@gmail.com>
2018-10-13 19:46:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
-    "This one is on me!";
+    return "This one is on me!";
   } else if (distance > 2000 && distance <= 2500){
       "I will gladly take your thirty bucks!";
   } else if ( distance > 2500) {
       "No can do."
     }
   }
-  
